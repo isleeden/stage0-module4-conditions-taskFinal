6a00ec1b5e837892077cee29d56efaabdaaf1a2f
isleeden <nkoreanarmy@gmail.com>
2022-08-07 17:13:42 +0500

    implement final task

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -1,7 +1,7 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
-    public void validate(double firstSide, double secondSide, double thirdSide) {
-
+    public void validate(double a, double b, double c) {
+        System.out.println(a + b > c && a + c > b && c + b > a ? "this is a valid triangle" : "it's not a triangle");
     }
 }
